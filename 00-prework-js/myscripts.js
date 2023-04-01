cbfd7a5d3a98af5fa2535fcfec2070b6ecd48ab6
Olivier Głowacki <glowacki.olivier@gmail.com>
2023-04-01 15:42:41 +0200

    rozwiązanie

---
+++ 00-prework-js/myscripts.js
@@ -0,0 +1,29 @@
+const products = [
+    {
+        name: 'banan',
+        price: 4.99
+    },
+    {
+        name: "chleb",
+        price: 3.25
+    },
+    {
+        name: 'ser',
+        price: 7.00
+    },
+    {
+        name: 'baton',
+        price: 1.99
+    }
+];
+
+let sum = 0;
+let avr = 0;
+
+for (let  i=0; i<products.length; i++){
+    sum += products[i].price;
+}
+avr = sum/products.length;
+
+console.log(`łączna cena produktów: ${sum}`);
+console.log(`średnia cena produktu: ${avr}`);
